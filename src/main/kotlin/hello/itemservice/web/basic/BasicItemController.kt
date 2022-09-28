55eaa705a7f5813d3fab6287f18a843fb0a38006
wafflejuice <wafflejuice96@gmail.com>
2022-09-28 23:43:46 +0900

    상품 목록 - thymeleaf

---
+++ src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
@@ -0,0 +1,28 @@
+package hello.itemservice.web.basic
+
+import hello.itemservice.domain.item.Item
+import hello.itemservice.domain.item.ItemRepository
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/basic/items")
+class BasicItemController(
+    val itemRepository: ItemRepository
+) {
+    @GetMapping
+    fun items(model: Model): String {
+        val items = itemRepository.findAll()
+        model.addAttribute("items", items)
+        return "basic/items"
+    }
+
+    @PostMapping
+    fun init() {
+        itemRepository.save(Item(itemName = "itemA", price = 10000, quantity = 10))
+        itemRepository.save(Item(itemName = "itemB", price = 20000, quantity = 20))
+    }
+}
